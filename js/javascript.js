85c607617d7403bd021b6efb771ad04b5778e6e7
tsuji_takuya <tsuji_takuya@cyberagent.co.jp>
2016-04-25 00:42:06 +0900

    add file

---
+++ js/javascript.js
@@ -0,0 +1,32 @@
+// cssファイルの読み込み
+var style = document.createElement('link');
+style.rel = 'stylesheet';
+style.type = 'text/css';
+style.href = chrome.extension.getURL('css/style.css');
+(document.head || document.documentElement).appendChild(style);
+
+$(function() {
+    // 設定
+    var $width = 640; // 横幅
+    var $height = 300; // 高さ
+    var $interval = 3000; // 切り替わりの間隔（ミリ秒）
+    var $fade_speed = 1000; // フェード処理の早さ（ミリ秒）
+    $("#slide ul li").css({
+        "position": "relative",
+        "overflow": "hidden",
+        "width": $width,
+        "height": $height
+    });
+    $("#slide ul li").hide().css({
+        "position": "absolute",
+        "top": 0,
+        "left": 0
+    });
+    $("#slide ul li:first").addClass("active").show();
+    setInterval(function() {
+        var $active = $("#slide ul li.active");
+        var $next = $active.next("li").length ? $active.next("li") : $("#slide ul li:first");
+        $active.fadeOut($fade_speed).removeClass("active");
+        $next.fadeIn($fade_speed).addClass("active");
+    }, $interval);
+});
